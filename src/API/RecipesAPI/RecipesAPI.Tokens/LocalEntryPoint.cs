51d1085be33a1b5388fe36685b4947dcda086885
Jibril Esa <jibril.esa@gmail.com>
2021-06-02 00:18:54 -0400

    2.0

--- src/API/RecipesAPI/RecipesAPI.Tokens/LocalEntryPoint.cs
+++ src/API/RecipesAPI/RecipesAPI.Tokens/LocalEntryPoint.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Amazon.Lambda;
 
 namespace RecipesAPI.Tokens
 {
@@ -13,9 +14,36 @@ namespace RecipesAPI.Tokens
     /// </summary>
     public class LocalEntryPoint
     {
-        public static void Main(string[] args)
+        // public static void Main(string[] args)
+        // {
+        //     BuildWebHost(args).Run();
+        // }
+
+
+        private static readonly LambdaEntryPoint LambdaEntryPoint = new LambdaEntryPoint();
+        private static readonly Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>> Func = LambdaEntryPoint.FunctionHandlerAsync;
+
+        public static async Task Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            try{
+            #if DEBUG
+                BuildWebHost(args).Run();
+            #else
+                //wrap the  FunctionHandler in a form that  LambdaBootstrap can work with
+                using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(Func, new DefaultLambdaJsonSerializer()))
+
+                //Instantiate a LambdaBootstrap and  run it 
+                using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+                {
+                    await bootstrap.RunAsync();
+                }
+            #endif
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message +"\n" + e.Source +"\n" + e.StackTrace);
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
